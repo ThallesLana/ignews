80038761b451e7ae374f35b7e5059ad26042ba23
ThallesLana <thalles.lana1@gmail.com>
2022-06-07 10:23:53 -0300

    init commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from "../../node_modules/next/app";
-import styles from "../../styles/styles.module.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
